72607eccfe00d004a68ab4ccc9abed7a14d16992
Giola-celeste Fritz <gioia-celeste.fritz@project-a.com>
2014-05-14 16:11:51 +0200

    experimenting with the secret_token and heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -11,7 +11,7 @@
 # if you're sharing your code publicly.
 require 'securerandom'
 
-=begin
+
 def secure_token
   token_file = Rails.root.join('.secret')
   if File.exist?(token_file)
@@ -24,7 +24,6 @@ def secure_token
     token
   end
 end
-=end
+
 
 SampleApp::Application.config.secret_key_base = 'f1d669cfc294b51d8ab81ceb1c6507821971acb4db7726b5bff321438a901bc6b045d4e2bb5fcdc934bd70fbd291b6421953ee1e6bdb16079a7e01ea805340b1'
-SampleApp::Application.config.secret_token = ENV['SECRET_TOKEN']
