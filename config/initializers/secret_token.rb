8d6039c4bf6907c8825c1cb06aff93e8a014b60d
Giola-celeste Fritz <gioia-celeste.fritz@project-a.com>
2014-05-14 14:56:27 +0200

    delete submodule git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,30 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+=begin
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+=end
+
+SampleApp::Application.config.secret_key_base = 'f1d669cfc294b51d8ab81ceb1c6507821971acb4db7726b5bff321438a901bc6b045d4e2bb5fcdc934bd70fbd291b6421953ee1e6bdb16079a7e01ea805340b1'
+SampleApp::Application.config.secret_token = ENV['SECRET_TOKEN']
